c80a2b700c2493e3817bb43c6a95791e19f5af09
mida-hub <rusuden0106@gmail.com>
2020-02-18 22:16:40 +0900

    simulation

---
+++ statistics/r/basic/ex_d01_03.R
@@ -0,0 +1,37 @@
+# ダイスシミュレーション
+dice_function <- function(n){
+  dice <- as.integer(runif(n, 1, 7))
+  dice.table <- table(dice)
+  print(dice.table)
+  print(prop.table(dice.table))
+  hist(dice,
+       breaks=c(0, 1, 2, 3, 4, 5, 6),
+       probability=T,
+       main="Histogram of 1-6",
+       xlab="dice",
+       col="gray")
+}
+
+dice_function(100)
+dice_function(1000)
+dice_function(10000)
+dice_function(100000)
+
+# 中心極限定理
+dice_mean_function <- function(n){
+  dice.mean <- c(NULL)
+  for (i in 1:n){
+    dice <- as.integer(runif(100, 1, 7))
+    dice.mean <- c(dice.mean, mean(dice))
+  }
+  dice.mean.table <- table(dice.mean)
+  print(mean(dice.mean))
+  print(var(dice.mean))
+  print(sd(dice.mean))
+  barplot(dice.mean.table)
+}
+
+dice_mean_function(100)
+dice_mean_function(1000)
+dice_mean_function(10000)
+dice_mean_function(100000)
