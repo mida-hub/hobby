09462142a641b14ccbcdfa9496652259d01a1b50
mida-hub <rusuden0106@gmail.com>
2020-02-21 21:29:45 +0900

    twitter&r

--- statistics/r/basic/ex_d01_03.R
+++ statistics/r/basic/ex_d01_03.R
@@ -17,6 +17,23 @@ dice_function(1000)
 dice_function(10000)
 dice_function(100000)
 
+dice_function2 <- function(n){
+  dice <- sample(6, n, replace=TRUE)
+  dice.table <- table(dice)
+  print(dice.table)
+  print(prop.table(dice.table))
+  barplot(prop.table(dice.table),
+          main="Barplot of 1-6",
+          xlab="dice",
+          ylab="probability",
+          col="gray")
+}
+
+dice_function2(100)
+dice_function2(1000)
+dice_function2(10000)
+dice_function2(100000)
+
 # 中心極限定理
 dice_mean_function <- function(n){
   dice.mean <- c(NULL)
@@ -35,3 +52,18 @@ dice_mean_function(100)
 dice_mean_function(1000)
 dice_mean_function(10000)
 dice_mean_function(100000)
+
+dice_mean_function2 <- function(n){
+  dice <- matrix(sample(6, n * 100, replace=TRUE), n)
+  dice.mean <- rowMeans(dice)
+  dice.mean.table <- table(dice.mean)
+  print(mean(dice.mean))
+  print(var(dice.mean))
+  print(sd(dice.mean))
+  barplot(dice.mean.table)
+}
+
+dice_mean_function2(100)
+dice_mean_function2(1000)
+dice_mean_function2(10000)
+dice_mean_function2(100000)
