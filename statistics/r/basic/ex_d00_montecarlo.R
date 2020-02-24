ce2bd232615fb0419f57da9e027d1ac63cea4348
mida-hub <rusuden0106@gmail.com>
2020-02-25 08:03:38 +0900

    modified

--- statistics/r/basic/ex_d00_montecarlo.R
+++ statistics/r/basic/ex_d00_montecarlo.R
@@ -3,17 +3,17 @@ mcs_and_plot_function <- function(N){
   # 円を描く
   # x^2 + y^2 = 1
   # y = sqrt(1 - x^2)
-  x = seq(0, 1, by=0.001)
-  y = sqrt(1 - x^2)
+  x <- seq(0, 1, by=0.001)
+  y <- sqrt(1 - x^2)
 
   # 乱数を発生させる
-  xmc = runif(N, 0, 1)
-  ymc = runif(N, 0, 1)
+  xmc <- runif(N, 0, 1)
+  ymc <- runif(N, 0, 1)
   
   # 円の範囲内か
-  threshold  = (xmc^2 + ymc^2) <= 1
+  threshold <- (xmc^2 + ymc^2) <= 1
   # color用
-  threshold_factor = as.factor(threshold)
+  threshold_factor <- as.factor(threshold)
 
   # 円の描画
   plot(x, y, xlim=c(0, 1), ylim=c(0, 1), xlab="x", ylab="y", type="l")
@@ -24,9 +24,9 @@ mcs_and_plot_function <- function(N){
   plot(xmc, ymc, xlim=c(0, 1), ylim=c(0, 1), xlab="x", ylab="y", col=threshold_factor, cex=0.1)
 
   # 円の面積
-  true_value = pi / 4
+  true_value <- pi / 4
   # モンテカルロ法
-  mcs = sum(threshold) / N
+  mcs <- sum(threshold) / N
   
   print(true_value)
   print(mcs)
@@ -40,13 +40,13 @@ mcs_ratio_function <- function(N){
   set.seed(12345)        
   mcs_ratio <- c(NULL)
   # 乱数を発生させる
-  xmc = runif(N, 0, 1)
-  ymc = runif(N, 0, 1)
+  xmc <- runif(N, 0, 1)
+  ymc <- runif(N, 0, 1)
   # 円の範囲内か
-  threshold  = (xmc^2 + ymc^2) <= 1
+  threshold <- (xmc^2 + ymc^2) <= 1
   for(i in 1:N){
-    mcs = sum(threshold[1:i]) / i
-    mcs_ratio = c(mcs_ratio, mcs)
+    mcs <- sum(threshold[1:i]) / i
+    mcs_ratio <- append(mcs_ratio, mcs)
   }
   # 描画
   plot(seq(1, N), mcs_ratio, xlab="N", ylab="value", type="l")
