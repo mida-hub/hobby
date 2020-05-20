8f5920ed9a96235224077df5d456f038e3b54898
mida-hub <rusuden0106@gmail.com>
2020-05-20 23:38:23 +0900

    14-16

---
+++ practice/terraform/05-16/14.3.tf
@@ -0,0 +1,26 @@
+data "aws_iam_policy_document" "codebuild" {
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+      "ecr:DescribeImages",
+      "ecr:BatchGetImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:CompleteLayerPart",
+      "ecr:PutImage",
+    ]
+  }
+}
