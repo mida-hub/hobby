d403cefa812fa1eceb6c9797ddd5a244d0106df0
mida-hub <rusuden0106@gmail.com>
2020-06-06 23:38:54 +0900

    class

---
+++ practice/python/071_080/071_class.py
@@ -0,0 +1,20 @@
+
+class Car:
+    """車クラス"""
+    country = "Japan"
+    year = 2019
+    name = "Prius"
+
+    def print_name(self):
+        print("print_name実行")
+        print(self.name)
+
+my_car = Car()
+print(my_car.year)
+my_car.print_name()
+
+list_a = ["apple", "banana", Car()]
+# second_car = list_a[2]()
+# second_car.print_name()
+list_a[2].print_name()
+help(Car)
