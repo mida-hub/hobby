6d0408bbf0fcc146a5415d926ea71661e367d00f
mida-hub <rusuden0106@gmail.com>
2020-07-24 21:56:47 +0900

    pug and heroku

--- practice/n_pre_school/beginner/chapter3/node-js-http/index.js
+++ practice/n_pre_school/beginner/chapter3/node-js-http/index.js
@@ -1,5 +1,6 @@
 'use strict';
 const http = require('http');
+const pug = require('pug');
 const server = http
     .createServer((req, res) => {
         const now = new Date();
@@ -12,9 +13,29 @@ const server = http
         switch (req.method) {
             case 'GET':
                 // res.write('GET ' + req.url);
-                const fs = require('fs');
-                const rs = fs.createReadStream('./form.html');
-                rs.pipe(res);
+                // const fs = require('fs');
+                // const rs = fs.createReadStream('./form.html');
+                // rs.pipe(res);
+                if (req.url === '/enquetes/yaki-shabu') {
+                    res.write(pug.renderFile('./form.pug', {
+                        path: req.url,
+                        firstItem: '焼き肉',
+                        secondItem: 'しゃぶしゃぶ'
+                    }));
+                } else if (req.url === '/enquetes/rice-bread') {
+                    res.write(pug.renderFile('./form.pug', {
+                        path: req.url,
+                        firstItem: 'ごはん',
+                        secondItem: 'パン'
+                    }));
+                } else if (req.url === '/enquetes/sushi-pizza') {
+                    res.write(pug.renderFile('./form.pug', {
+                        path: req.url,
+                        firstItem: '寿司',
+                        secondItem: 'ピザ'
+                    }));
+                }
+                res.end();
                 break;
             case 'POST':
                 // res.write('POST ' + req.url);
@@ -26,7 +47,8 @@ const server = http
                     const decoded = decodeURIComponent(rawData);
                     const qs = require('querystring');
                     const answer = qs.parse(decoded);
-                    const res_answer = answer['name'] + 'さんは' + answer['yaki-shabu'] + 'に投票しました';
+                    // const res_answer = answer['name'] + 'さんは' + answer['yaki-shabu'] + 'に投票しました';
+                    const res_answer = answer['name'] + 'さんは' + answer['favorite'] + 'に投票しました';
                     console.info('[' + now + ']: ' + decoded + ' => ' + res_answer);
                     res.write('<!DOCTYPE html><html lang="ja"><body><h1>' + res_answer + '</h1></body></html>');
                     res.end();
@@ -45,7 +67,7 @@ const server = http
     .on('clientError', e => {
         console.error('[' + new Date() + '] Client Error', e);
     });
-const port = 8000;
+const port = process.env.PORT || 8000;
 server.listen(port, () => {
     console.info('[' + new Date() + '] Listening on ' + port);
 });
