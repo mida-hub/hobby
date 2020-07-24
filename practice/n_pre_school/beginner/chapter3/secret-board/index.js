3eda19bb914fe8e588606253545fbe449b33853d
mida-hub <rusuden0106@gmail.com>
2020-07-25 00:47:37 +0900

    掲示板

---
+++ practice/n_pre_school/beginner/chapter3/secret-board/index.js
@@ -0,0 +1,26 @@
+'use strict';
+const http = require('http');
+const auth = require('http-auth');
+const router = require('./lib/router');
+
+const basic = auth.basic({
+  realm: 'Enter username and password.',
+  file: './users.htpasswd'
+});
+
+// const server = http.createServer((req, res) => {
+const server = http.createServer(basic, (req, res) => {
+    // res.end('hi');
+    router.route(req, res);
+  })
+  .on('error', e => {
+    console.error('Server Error', e);
+  })
+  .on('clientError', e => {
+    console.error('Client Error', e);
+  });
+
+const port = 8000;
+server.listen(port, () => {
+  console.info('Listening on ' + port);
+});
