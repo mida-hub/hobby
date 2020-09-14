8fab370b9a212353a93a6b61bf97f40ef7fd0f03
mida-hub <rusuden0106@gmail.com>
2020-09-14 11:19:18 +0900

    remove

--- practice/vuejs/ex08-10/kanban-app_backup/build/dev-server.js
+++
@@ -1,8 +0,0 @@
-// Node.jsのrequireスタイルでインポート
-const bodyParser = require('body-parser')
-
-// `Express`アプリケーションインスタンスを受け取る関数をエクスポート
-module.exports = app => {
-  // HTTPリクエストのbodyの内容をJSONとして解析するようミドルウェアをインストール
-  app.use(bodyParser.json())
-}
