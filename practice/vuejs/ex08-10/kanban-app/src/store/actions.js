686f4f28a4e236482ec507763c1c4075bcd9da6f
mida-hub <rusuden0106@gmail.com>
2020-09-15 10:53:44 +0900

    更新

--- practice/vuejs/ex08-10/kanban-app/src/store/actions.js
+++ practice/vuejs/ex08-10/kanban-app/src/store/actions.js
@@ -1,30 +1,39 @@
-/* eslint-disalbe no-unused-vars */
-import * as types from './mutation-type'
+/* eslint-disable no-unused-vars */
+import * as types from './mutation-types'
 import { Auth, List, Task } from '../api'
 /* eslint-enable no-unused-vars */
 
 export default {
-  login: ({ commit }) => {
-    throw new Error('login action should be implemented')
+  login: ({ commit }, authInfo) => {
+    return Auth.login(authInfo)
+      .then(({ token, userId }) => {
+        commit(types.AUTH_LOGIN, { token, userId })
+      })
+      .catch(err => { throw err })
   },
 
   fetchLists: ({ commit }) => {
+    // TODO:
     throw new Error('fetchLists action should be implemented')
   },
 
   addTask: ({ commit }) => {
+    // TODO:
     throw new Error('addTask action should be implemented')
   },
 
   updateTask: ({ commit }) => {
+    // TODO:
     throw new Error('updateTask action should be implemented')
   },
 
   removeTask: ({ commit }) => {
+    // TODO:
     throw new Error('removeTask action should be implemented')
   },
 
-  logoutTask: ({ commit }) => {
-    throw new Error('logoutTask action should be implemented')
+  logout: ({ commit }) => {
+    // TODO:
+    throw new Error('logout action should be implemented')
   }
 }
