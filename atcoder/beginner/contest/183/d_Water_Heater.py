2b56c681ebcba0cc570f00ff6345bbe880edf99a
mida-hub <rusuden0106@gmail.com>
2020-11-17 22:28:30 +0900

    method-imosu

--- atcoder/beginner/contest/183/d_Water_Heater.py
+++ atcoder/beginner/contest/183/d_Water_Heater.py
@@ -1,13 +1,34 @@
 def main():
-    import numpy as np
     n, w = map(int, input().split())
 
-    plan = np.zeros(10**5 * 2)
+    sp_dict = {}
+
     for i in range(n):
         s, t, p = map(int, input().split())
-        plan[s-1:t-1] += p
+        if sp_dict.get(s) is not None:
+            sp_dict[s] += p
+        else:
+            sp_dict[s] = p
 
-    if any(plan > w):
+        if sp_dict.get(t) is not None:
+            sp_dict[t] -= p
+        else:
+            sp_dict[t] = -p
+
+    is_result = False
+    total = 0
+
+    # print(sorted(sp_dict.items()))
+
+    # simulation
+    for key, value in sorted(sp_dict.items()):
+        # print(value)
+        total += value
+        if total > w:
+            is_result = True
+            break
+    
+    if is_result:
         print('No')
     else:
         print('Yes')
