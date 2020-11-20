8d405f57cda382365cf077905e7b45b8e14c3192
mida-hub <rusuden0106@gmail.com>
2020-11-20 22:47:27 +0900

    181

--- atcoder/beginner/contest/181/C_Collinearity.py
+++ atcoder/beginner/contest/181/C_Collinearity.py
@@ -1,31 +1,17 @@
-from decimal import Decimal
-
 def IsCollinear(a, b, c):
-    a0 = Decimal(str(a[0]))
-    a1 = Decimal(str(a[1]))
-    b0 = Decimal(str(b[0]))
-    b1 = Decimal(str(b[1]))
-    c0 = Decimal(str(c[0]))
-    c1 = Decimal(str(c[1]))
+    a0 = a[0]
+    a1 = a[1]
+    b0 = b[0]
+    b1 = b[1]
+    c0 = c[0]
+    c1 = c[1]
 
-    ab = ((b0 - a0) ** Decimal('2') + (b1 - a1) ** Decimal('2')) ** Decimal('0.5')
-    ac = ((c0 - a0) ** Decimal('2') + (c1 - a1) ** Decimal('2')) ** Decimal('0.5')
-    bc = ((c0 - b0) ** Decimal('2') + (c1 - b1) ** Decimal('2')) ** Decimal('0.5')
+    b0 -= a0
+    b1 -= a1
+    c0 -= a0
+    c1 -= a1
 
-    # ab = ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** 0.5
-    # ac = ((c[0] - a[0]) ** 2 + (c[1] - a[1]) ** 2) ** 0.5
-    # bc = ((c[0] - b[0]) ** 2 + (c[1] - b[1]) ** 2) ** 0.5
-
-    # print(ab)
-    # print(ac)
-    # print(bc)
-
-    if any([(ab + ac == bc),
-            (ab + bc == ac),
-            (ac + bc == ab)]):
-        return True
-    else:
-        return False
+    return b0 * c1 == b1 * c0
 
 n = int(input())
 xn = []
@@ -41,12 +27,9 @@ c_list = [x for x in range(n)]
 
 isExist = False
 for v in itertools.combinations(c_list, 3):
-    # print(v)
     a = [xn[v[0]], yn[v[0]]]
     b = [xn[v[1]], yn[v[1]]]
     c = [xn[v[2]], yn[v[2]]]
-    # print(a, b, c)
-    # print(IsCollinear(a, b, c))
     isExist = IsCollinear(a, b, c)
     if isExist:
         break
