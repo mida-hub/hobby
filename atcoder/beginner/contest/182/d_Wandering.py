f8bba12117aa7b1e640ac7e7a08554a80371a703
mida-hub <rusuden0106@gmail.com>
2020-11-20 23:36:07 +0900

    182

--- atcoder/beginner/contest/182/d_Wandering.py
+++ atcoder/beginner/contest/182/d_Wandering.py
@@ -1,28 +1,21 @@
-import numpy as np
 n = int(input())
-an = np.array(list(map(int, input().split())))
-max_a_n = np.zeros(len(an))
-rui = np.zeros(len(an))
+an = list(map(int, input().split()))
 
-total = 0
-max_point = 0
-for i, a in enumerate(an):
-    if i == 0:
-        rui[i] = a
-        total = a
-        max_a_n[i] = max([0, a])
-        if total > 0:
-            max_point = total
-    else:
-        rui[i] = rui[i-1] + a
-        max_a_n[i] = an[0:i+1].sum()
-        if total + max_a_n.max() > total:
-            max_point = total + max_a_n.max()
-        # print(max_point)
-        # print(max_a_n, max_a_n.max())
-        total += rui[i].sum()
-        # print(total)
-        # print(max_a_n)
-# print(rui)
-# print(total)
-print(int(max_point))
+p = 0   # 座標の変化
+q = 0   # 開始から終了までの最大値
+r = 0   # 座標の最大値
+x = 0   # 動作結果
+for i in range(n):
+    p += an[i]
+    q = max([p, q])
+    r = max([r, x + q])
+    x += p
+
+    # print('-'*10)
+    # print(f'a:{an[i]}')
+    # print(f'p:{p}')
+    # print(f'q:{q}')
+    # print(f'r:{r}')
+    # print(f'x:{x}')
+
+print(r)
