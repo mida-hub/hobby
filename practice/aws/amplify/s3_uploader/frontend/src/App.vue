9e05e9b1d1e514cf7983981c40548000e45d2e0d
mida-hub <rusuden0106@gmail.com>
2020-12-23 23:22:02 +0900

    vue dropzone

---
+++ practice/aws/amplify/s3_uploader/frontend/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    S3 Uploader
+    <vue-dropzone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions"></vue-dropzone>
+  </div>
+</template>
+
+<script>
+import vue2Dropzone from 'vue2-dropzone'
+import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+
+export default {
+  name: "App",
+  components: {
+    vueDropzone: vue2Dropzone
+  },
+  data: function () {
+    return {
+      dropzoneOptions: {
+          url: 'https://httpbin.org/post',
+          chunking: true,
+          chunkSize: 500,
+          thumbnailWidth: 150,
+          thumbnailHeight: 150,
+          maxFilesize: 50,
+          maxFiles: 1,
+          addRemoveLinks: true,
+          headers: { "My-Awesome-Header": "header value" },
+          dictDefaultMessage: "<i class='fa fa-cloud-upload'></i>Drop Here. File Upload"
+      }
+    }
+  }
+};
+</script>
+
+<style>
+@import url("https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
