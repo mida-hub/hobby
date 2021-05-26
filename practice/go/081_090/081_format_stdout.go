41daabb438b3cdd8d311db20765082a81f60a1a1
mida-hub <rusuden0106@gmail.com>
2021-05-26 18:28:08 +0900

    go format

---
+++ practice/go/081_090/081_format_stdout.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("東京", "の降水確率は", 90, "%です。\n")
+	fmt.Println("東京", "の降水確率は", 90, "%です。")
+	fmt.Printf("%sの降水確率は%d%%です。", "東京", 90)
+}
