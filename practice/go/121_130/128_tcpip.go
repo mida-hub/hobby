97b96a4d5d2866b2617dbbd6b094ff0862a3d849
mida-hub <rusuden0106@gmail.com>
2021-05-29 22:16:50 +0900

    http

---
+++ practice/go/121_130/128_tcpip.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "golang.jp:80")
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer func() {
+		conn.Close()
+	}()
+	fmt.Fprint(conn, "GET /hello.html HTTP/1.1\r\n")
+	fmt.Fprint(conn, "HOST golang.jp\r\n")
+	fmt.Fprint(conn, "\r\n")
+
+	response, err := ioutil.ReadAll(conn)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(response))
+}
