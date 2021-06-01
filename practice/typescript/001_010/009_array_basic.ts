8887aae92e74b70109b710a9a8907de39a0a9dd7
mida-hub <rusuden0106@gmail.com>
2021-06-01 23:01:08 +0900

    typescript basic

---
+++ practice/typescript/001_010/009_array_basic.ts
@@ -0,0 +1,4 @@
+{
+    let data: string[] = ['Java', 'PHP', 'Ruby', 'C#'];
+    console.log(data[0]);
+}
---
+++ practice/typescript/001_010/010_array_readonly.ts
@@ -0,0 +1,4 @@
+{
+    let data: readonly string[] = ['Java', 'PHP', 'Ruby', 'C#'];
+    // data[1] = 'Python';
+}
