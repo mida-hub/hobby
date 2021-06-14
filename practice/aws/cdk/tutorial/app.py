3ae64f26bfb06b76d77f3efb51a1e7aa842c8f7e
mida-hub <rusuden0106@gmail.com>
2021-06-14 23:16:29 +0900

    fix

--- practice/aws/cdk/tutorial/app.py
+++ practice/aws/cdk/tutorial/app.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
 
 from aws_cdk import core
-from aws_cdk import aws_s3 as s3
-class S3Stack(core.Stack):
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
-        bucket = s3.Bucket(self, 
-            "MyFirstBucket", 
-            versioned=True,)
+from s3_stack import S3Stack
 
 app = core.App()
 S3Stack(app, "S3Stack")
