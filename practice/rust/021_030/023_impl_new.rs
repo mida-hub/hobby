f03f0f3eba05296e7240037641c7fc34497171bf
mida-hub <rusuden0106@gmail.com>
2021-06-26 00:11:56 +0900

    rust impl

---
+++ practice/rust/021_030/023_impl_new.rs
@@ -0,0 +1,28 @@
+struct Person {
+    name: String,
+    age: u32,
+}
+
+impl Person {
+    fn new(name: &str, age: u32) -> Person {
+        Person {
+            name: String::from(name),
+            age: age,
+        }
+    }
+
+    fn say_name(&self) -> &Self {
+        println!("I am {}.", self.name);
+        self
+    }
+
+    fn say_age(&self) -> &Self {
+        println!("I am {} year(s) old.", self.age);
+        self
+    }
+}
+
+fn main() {
+    let p = Person::new("taro", 20);
+    p.say_name().say_age();
+}
