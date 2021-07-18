4ed63b73c15dd3b1b608c17185128fab9d20160a
mida-hub <rusuden0106@gmail.com>
2021-07-19 01:19:14 +0900

    abc194c

--- atcoder/beginner/contest/101_200/194/C_Squared_Error.py
+++ atcoder/beginner/contest/101_200/194/C_Squared_Error.py
@@ -1,13 +1,22 @@
 def main():
-    import numpy as np
-
     n = int(input())
     an = list(map(int, input().split()))
-    base = np.array(an)
-    base_com = []
 
-    for i in range(n):
-        base_com = np.concatenate([base_com, base[i+1:] - an[i]], 0)
-    print(int(sum(base_com*base_com)))
+    cnt = [0] * 401
+
+    an_abs = []
+    for a in an:
+        cnt[a+200] += 1
+        an_abs.append(a+200)
+    an_abs = sorted(set(an_abs))
+    
+    total = 0
+    for a in an_abs:
+        for b in an_abs:
+            if a == b: continue
+            if a < b: continue
+            # print(a, b)
+            total += (b - a) ** 2 * cnt[a] * cnt[b]
+    print(total)
 
 main()
