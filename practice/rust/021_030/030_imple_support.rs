f6ad435740b9d6f8dbbe1bba2f2c2f07ef5ca49f
mida-hub <rusuden0106@gmail.com>
2021-09-16 23:28:35 +0900

    rust macro

---
+++ practice/rust/021_030/030_imple_support.rs
@@ -0,0 +1,36 @@
+enum Emotion {
+    Anger,
+    Happy,
+}
+
+trait Emotional {
+    fn get_happy(&mut self) -> String;
+    fn get_anger(&mut self) -> String;
+    fn tell_state(&self) -> String;
+}
+
+struct HappyPerson {
+    name: String,
+    state: Emotion,
+}
+
+impl Emotional for HappyPerson {
+    fn get_anger(&mut self) -> String {
+        unimplemented!()
+    }
+    fn get_happy(&mut self) -> String {
+        format!("{} is always happy.", self.name)
+    }
+    fn tell_state(&self) -> String {
+        todo!()
+    }
+}
+
+fn main() {
+    let mut p = HappyPerson {
+        name: "Takashi".to_string(),
+        state: Emotion::Happy,
+    };
+
+    println!("{}", p.get_happy());
+}
