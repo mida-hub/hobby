79abee26218f304d7cb2f3a06063221ea5d75495
mida-hub <rusuden0106@gmail.com>
2021-09-21 17:56:51 +0900

    sh input

---
+++ practice/shell/001_010/004.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+check=""
+while [ -z "$check" ]; do
+    echo "Chose your favorite food"
+    echo " 1. sushi"
+    echo " 2. tempura"
+    echo " 710. natto"
+
+    echo -n "Input (1, 2, 710) ?"
+
+    read check
+
+    case $check in
+        1)
+            echo "Me too!"
+            ;;
+        2)
+            echo "Be careful when you eat with the watemelon."
+            ;;
+        710)
+            echo "Really!?"
+            ;;
+        *)
+            echo "*** Bad choice!"
+            printf '¥007'
+            check=""
+            ;;
+    esac
+done
