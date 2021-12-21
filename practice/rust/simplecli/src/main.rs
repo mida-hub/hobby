d23829a96bfa147972bca34b7ec6b528d1d03aaf
mida-hub <rusuden0106@gmail.com>
2021-12-22 00:38:27 +0900

    rust err handle

--- practice/rust/simplecli/src/main.rs
+++ practice/rust/simplecli/src/main.rs
@@ -1,3 +1,5 @@
+use anyhow::{bail, ensure, Context, Result};
+
 use clap::{App, Arg};
 use std::fs::File;
 use std::io::{stdin, BufRead, BufReader};
@@ -9,20 +11,23 @@ impl RpnCalculator {
         Self(verbose)
     }
 
-    pub fn eval(&self, formula: &str) -> i32 {
+    pub fn eval(&self, formula: &str) -> Result<i32> {
         let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
         self.eval_inner(&mut tokens)
     }
 
-    fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+    fn eval_inner(&self, tokens: &mut Vec<&str>) -> Result<i32> {
         let mut stack = Vec::new();
+        let mut pos = 0;
 
         while let Some(token) = tokens.pop() {
+            pos += 1;
+
             if let Ok(x) = token.parse::<i32>() {
                 stack.push(x);
             } else {
-                let y = stack.pop().expect("invalid syntax");
-                let x = stack.pop().expect("invalid syntax");
+                let y = stack.pop().context(format!("invalid syntax at {}", pos))?;
+                let x = stack.pop().context(format!("invalid syntax at {}", pos))?;
 
                 let res = match token {
                     "+" => x + y,
@@ -30,7 +35,7 @@ impl RpnCalculator {
                     "*" => x * y,
                     "/" => x / y,
                     "%" => x % y,
-                    _ => panic!("invalid token"),
+                    _ => bail!("invalid token at {}", pos),
                 };
                 stack.push(res);
             }
@@ -40,15 +45,13 @@ impl RpnCalculator {
             }
         }
 
-        if stack.len() == 1 {
-            stack[0]
-        } else {
-            panic!("invalid syntax")
-        }
+        ensure!(stack.len() == 1, "invalid syntax");
+
+        Ok(stack[0])
     }
 }
 
-fn main() {
+fn main() -> Result<()> {
     let matches = App::new("My RPN program")
                     .version("1.0.0")
                     .author("Your name")
@@ -71,27 +74,32 @@ fn main() {
     let verbose = matches.is_present("verbose");
     match matches.value_of("formula_file"){
         Some(file) => {
-            let f = File::open(file).unwrap();
+            let f = File::open(file)?;
             let reader = BufReader::new(f);
-
             run(reader, verbose)
         }
         None => {
             let stdin = stdin();
             let reader = stdin.lock();
-            run(reader, verbose);
+            run(reader, verbose)
         }
     }
+
+    
 }
 
-fn run<R: BufRead>(reader: R, verbose: bool) {
+fn run<R: BufRead>(reader: R, verbose: bool) -> Result<()> {
     let calc = RpnCalculator::new(verbose);
 
     for line in reader.lines() {
-        let line = line.unwrap();
-        let answer = calc.eval(&line);
-        println!("{}", answer);
+        let line = line?;
+        match calc.eval(&line){
+            Ok(answer) => println!("{}", answer),
+            Err(e) => eprintln!("{:#?}", e),
+        }
     }
+
+    Ok(())
 }
 
 #[cfg(test)]
