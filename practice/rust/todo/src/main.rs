82aecd82c7dd9a9b9d0b9b2c5a38244b639c4051
mida-hub <rusuden0106@gmail.com>
2021-12-26 00:30:30 +0900

    rust sqlite select

--- practice/rust/todo/src/main.rs
+++ practice/rust/todo/src/main.rs
@@ -1,5 +1,5 @@
 use actix_rt;
-use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use actix_web::{get, web, App, HttpResponse, HttpServer, ResponseError};
 use askama::Template;
 use thiserror::Error;
 use r2d2::Pool;
@@ -21,21 +21,39 @@ struct IndexTemplate {
 enum MyError {
     #[error("Failed to render HTML")]
     AskamaError(#[from] askama::Error),
+
+    #[error("Failed to get connection")]
+    ConnectionPoolError(#[from] r2d2::Error),
+
+    #[error("Failed SQL execution")]
+    SQLiteError(#[from] rusqlite::Error),
 }
 
 impl ResponseError for MyError {}
 
 #[get("/")]
-async fn index() -> Result<HttpResponse, MyError> {
+async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    let mut statement = conn.prepare("SELECT id, text FROM todo")?;
+
+    let rows = statement.query_map(params![], |row| {
+        let id = row.get(0)?;
+        let text = row.get(1)?;
+        Ok(TodoEntry { id, text })
+    })?;
+
     let mut entries = Vec::new();
-    entries.push(TodoEntry {
-        id: 1,
-        text: "First entry".to_string(),
-    });
-    entries.push(TodoEntry {
-        id: 2,
-        text: "Second entry".to_string(),
-    });
+    for row in rows {
+        entries.push(row?);
+    }
+    // entries.push(TodoEntry {
+    //     id: 1,
+    //     text: "First entry".to_string(),
+    // });
+    // entries.push(TodoEntry {
+    //     id: 2,
+    //     text: "Second entry".to_string(),
+    // });
     let html = IndexTemplate { entries };
     let response_body = html.render()?;
     Ok(HttpResponse::Ok()
