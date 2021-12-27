8210b6fb0e94e77eecdddb63e6a0b958e2965a96
mida-hub <rusuden0106@gmail.com>
2021-12-28 00:06:42 +0900

    rust todo with docker

--- practice/rust/todo/README.md
+++ practice/rust/todo/README.md
@@ -5,3 +5,9 @@ cargo add thiserror
 cargo add askama
 cargo add r2d2 r2d2_sqlite
 ```
+
+# docker
+```
+docker build -t todo-app .
+docker run -p 8080:8080 todo-app
+```
--- practice/rust/todo/src/main.rs
+++ practice/rust/todo/src/main.rs
@@ -1,10 +1,23 @@
 use actix_rt;
-use actix_web::{get, web, App, HttpResponse, HttpServer, ResponseError};
+use actix_web::{get, web, App, 
+                HttpResponse, HttpServer, ResponseError,
+                http::header, post};
 use askama::Template;
 use thiserror::Error;
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 use rusqlite::params;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct AddParams {
+    text: String,
+}
+
+#[derive(Deserialize)]
+struct DeleteParams {
+    id: u32,
+}
 
 struct TodoEntry {
     id: u32,
@@ -31,6 +44,30 @@ enum MyError {
 
 impl ResponseError for MyError {}
 
+#[post("/add")]
+async fn add_todo(
+    params: web::Form<AddParams>,
+    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("INSERT INTO todo (text) VALUES (?)", &[&params.text])?;
+    Ok(HttpResponse::SeeOther()
+    .header(header::LOCATION, "/")
+    .finish())
+}
+
+#[post("/delete")]
+async fn delete_todo(
+    params: web::Form<DeleteParams>,
+    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("DELETE FROM todo WHERE id = ?", &[&params.id])?;
+    Ok(HttpResponse::SeeOther()
+    .header(header::LOCATION, "/")
+    .finish())
+}
+
 #[get("/")]
 async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpResponse, MyError> {
     let conn = db.get()?;
@@ -46,14 +83,7 @@ async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpRespo
     for row in rows {
         entries.push(row?);
     }
-    // entries.push(TodoEntry {
-    //     id: 1,
-    //     text: "First entry".to_string(),
-    // });
-    // entries.push(TodoEntry {
-    //     id: 2,
-    //     text: "Second entry".to_string(),
-    // });
+
     let html = IndexTemplate { entries };
     let response_body = html.render()?;
     Ok(HttpResponse::Ok()
@@ -77,7 +107,13 @@ async fn main() -> Result<(), actix_web::Error> {
     )
     .expect("Failed to create a table `todo`.");
 
-    HttpServer::new(move || App::new().service(index).data(pool.clone()))
+    HttpServer::new(move || {
+            App::new()
+            .service(index)
+            .service(add_todo)
+            .service(delete_todo)
+            .data(pool.clone())
+        })
         .bind("0.0.0.0:8080")?
         .run()
         .await?;
