15ac8f6548638f48a1fbbb66f820d0204508f9b0
mida-hub <rusuden0106@gmail.com>
2022-01-11 21:29:14 +0900

    abc234 c

--- atcoder/rust/beginner/contest/abc234/src/bin/c.rs
+++ atcoder/rust/beginner/contest/abc234/src/bin/c.rs
@@ -1,3 +1,22 @@
+fn to_bin(v: i64) -> String {
+    format!("{:b}", v).to_string()
+}
+
+fn to_int(s: String) -> i64 {
+    s.parse::<i64>().unwrap()
+}
+
 fn main() {
-    todo!();
+    proconio::input! {
+        k: i64,
+    };
+
+    let str_bin_k = to_bin(k);
+    let mut ans = "".to_string();
+    for s in str_bin_k.chars() {
+        // println!("{}", s);
+        // 文字列から整数変換->2倍する->文字列に直す
+        ans.push_str(&(2 * to_int(s.to_string())).to_string());
+    }
+    println!("{}", ans);
 }
