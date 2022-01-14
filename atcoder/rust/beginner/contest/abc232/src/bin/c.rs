6b35c5f3e99fb6ef289301e118e4a096a038fdcc
mida-hub <rusuden0106@gmail.com>
2022-01-15 00:03:13 +0900

    abc232 c

--- atcoder/rust/beginner/contest/abc232/src/bin/c.rs
+++ atcoder/rust/beginner/contest/abc232/src/bin/c.rs
@@ -1,3 +1,51 @@
+use itertools::Itertools;
+
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        m: usize,
+        ab: [[usize; 2]; m],
+        cd: [[usize; 2]; m],
+    }
+
+    // println!("{},{}", n, m);
+    // println!("{:?}", ab);
+    // println!("{:?}", cd);
+
+    // ボールの対応表を作成
+    let mut ab_matrix = vec![vec![false; n]; n];
+    let mut cd_matrix = vec![vec![false; n]; n];
+
+    for abi in &ab {
+        let ai = abi[0] - 1;
+        let bi = abi[1] - 1;
+        ab_matrix[ai][bi] = true;
+        ab_matrix[bi][ai] = true;
+    }
+    // println!("{:?}", ab_matrix);
+
+    for cdi in &cd {
+        let ci = cdi[0] - 1;
+        let di = cdi[1] - 1;
+        cd_matrix[ci][di] = true;
+        cd_matrix[di][ci] = true;
+    }
+    // println!("{:?}", cd_matrix);
+
+    for perm in (0..n).permutations(n) {
+        let mut ans = true;
+        for i in 0..n {
+            for j in 0..n {
+                if ab_matrix[i][j] != cd_matrix[perm[i]][perm[j]] {
+                    ans = false;
+                }
+            }
+        }
+        if ans {
+            // println!("{:?}", perm);
+            println!("Yes");
+            return
+        }
+    }
+    println!("No");
 }
