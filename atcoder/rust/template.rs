cee3b3ecdb5afb5892959ca2fa535609677d78a5
mida-hub <rusuden0106@gmail.com>
2022-01-16 23:58:15 +0900

    rust template

--- atcoder/rust/template.rs
+++ atcoder/rust/template.rs
@@ -5,3 +5,12 @@ fn to_bin(v: i64) -> String {
 fn to_int(s: String) -> i64 {
     s.parse::<i64>().unwrap()
 }
+
+// 二重配列 abc233 c
+let mut ans = Vec::new();
+let v = Vec::new();
+ans.push(v);
+ans[0].push(1);
+
+// hashmap
+// https://yiskw713.hatenablog.com/entry/rust-hashmap-sort
