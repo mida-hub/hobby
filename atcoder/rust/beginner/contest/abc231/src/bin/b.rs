0e97dd86be81279d853f8bace354e59a67eb166e
mida-hub <rusuden0106@gmail.com>
2022-01-16 23:58:26 +0900

    abc231 b

--- atcoder/rust/beginner/contest/abc231/src/bin/b.rs
+++ atcoder/rust/beginner/contest/abc231/src/bin/b.rs
@@ -1,3 +1,23 @@
+use std::collections::HashMap;
+
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        sn: [String; n],
+    }
+
+    // println!("{}", n);
+    // println!("{:?}", sn);
+
+    let mut votes: HashMap<&str, i32> = HashMap::new();
+    for s in &sn {
+        // key, value の組み合わせでカウントする
+        *votes.entry(s).or_insert(0) += 1;
+    }
+    // println!("{:?}", votes);
+    let mut votes_vec: Vec<(&&str, &i32)> = votes.iter().collect();
+    // value の降順にする
+    votes_vec.sort_by(|a, b| (-a.1).cmp(&(-b.1)));
+    // println!("{:?}", votes_vec);
+    println!("{}", votes_vec[0].0);
 }
