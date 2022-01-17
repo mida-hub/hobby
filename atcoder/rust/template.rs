504aa1c53ec4c973c6924408805f9be88e6c9011
mida-hub <rusuden0106@gmail.com>
2022-01-18 00:29:43 +0900

    rust template

--- atcoder/rust/template.rs
+++ atcoder/rust/template.rs
@@ -1,7 +1,9 @@
+//  binaryに変換する
 fn to_bin(v: i64) -> String {
     format!("{:b}", v).to_string()
 }
 
+// 数値型に変換する
 fn to_int(s: String) -> i64 {
     s.parse::<i64>().unwrap()
 }
@@ -14,9 +16,17 @@ ans[0].push(1);
 
 // hashmap sort
 // https://yiskw713.hatenablog.com/entry/rust-hashmap-sort
-
 let mut votes: HashMap<&str, i32> = HashMap::new();
 for s in &sn {
     // key, value の組み合わせでカウントする
     *votes.entry(s).or_insert(0) += 1;
 }
+
+// 文字列のn番目
+abc.chars().nth(0).unwrap()
+
+// 0埋め
+println!("{}", format!("AGC{:03}", ans));
+
+// 配列に要素が含まれる
+patterns.contains(&s_substr)
