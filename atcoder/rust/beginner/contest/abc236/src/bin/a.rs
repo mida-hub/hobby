d4735d7e5971954c207c1716f1aeb3456a662ca4
mida-hub <rusuden0106@gmail.com>
2022-01-26 00:21:55 +0900

    abc236

---
+++ atcoder/rust/beginner/contest/abc236/src/bin/a.rs
@@ -0,0 +1,11 @@
+use proconio::marker::{Chars, Usize1};
+
+fn main () {
+    proconio::input! {
+    mut s: Chars,
+        a: Usize1,
+        b: Usize1,
+    }
+    s.swap(a, b);
+    println!("{}", s.iter().collect::<String>());
+}
