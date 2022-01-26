6dac237baebde4df60295c013ed56cd36a672b3b
mida-hub <rusuden0106@gmail.com>
2022-01-26 23:40:08 +0900

    abc236 b

--- atcoder/rust/beginner/contest/abc236/src/bin/b.rs
+++ atcoder/rust/beginner/contest/abc236/src/bin/b.rs
@@ -1,3 +1,18 @@
+use std::collections::HashMap;
+
 fn main() {
-    todo!();
+    proconio::input! {
+            n: usize,
+            a4n: [usize; 4*n-1],
+        }
+
+    let mut check: HashMap<&usize, usize> = HashMap::new();
+
+    for a in &a4n {
+        *check.entry(a).or_insert(0) += 1;
+    }
+    // println!("{:?}", check);
+    let mut check_vec: Vec<(&&usize, &usize)> = check.iter().collect();
+    check_vec.sort_by(|a, b| a.1.cmp(&b.1));
+    println!("{}", check_vec[0].0);
 }
