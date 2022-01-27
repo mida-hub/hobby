cb40c08314be5c6c05abc11835366d67e91e368d
mida-hub <rusuden0106@gmail.com>
2022-01-27 23:41:22 +0900

    abc227 c

--- atcoder/rust/beginner/contest/abc227/src/bin/c.rs
+++ atcoder/rust/beginner/contest/abc227/src/bin/c.rs
@@ -1,3 +1,20 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+    }
+
+    // println!("{}", n);
+
+    let mut ans = 0;
+    for a in 1..=n {
+        if a * a * a > n { break; }
+        for b in a..=n {
+            if a * b * b > n { break; }
+            let c = n / (a * b);
+            if b <= c {
+                ans += c - b + 1;
+            }
+        }
+    }
+    println!("{}", ans);
 }
