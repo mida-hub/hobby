b0c89b2d563134b8ce68048cbdc96bed09f65278
mida-hub <rusuden0106@gmail.com>
2022-02-03 00:02:25 +0900

    abc229 c

--- atcoder/rust/beginner/contest/abc229/src/bin/c.rs
+++ atcoder/rust/beginner/contest/abc229/src/bin/c.rs
@@ -1,3 +1,32 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        mut w: usize,
+        mut abn: [[usize; 2]; n]
+    }
+
+    // println!("{},{}", n, w);
+    // println!("{:?}", abn);
+
+    abn.sort_by(|a, b| b.cmp(&a));
+    // println!("{:?}", abn);
+
+    let mut ans = 0;
+    for abi in &abn {
+        let a = abi[0];
+        let b = abi[1];
+
+        if w >= b {
+            w -= b;
+            ans += a * b;
+        } else {
+            ans += a * w;
+            w = 0;
+        }
+        // println!("a:{}, b:{}", a, b);
+        // println!("w:{}", w);
+        // println!("ans:{}", ans);
+    }
+
+    println!("{}", ans);
 }
