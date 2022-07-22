ef241c3bafd9bba3546599ac17575e2a9590f9d7
mida-hub <rusuden0106@gmail.com>
2022-07-22 23:55:04 +0900

    fix

--- practice/python/fastapi2/main.py
+++ practice/python/fastapi2/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 from starlette.responses import FileResponse
+from typing import Optional
+from pydantic import BaseModel
+
+class Blog(BaseModel):
+    title: str
+    description: str
+    published_at: Optional[bool]
+
 
 app = FastAPI()
 favicon_path = 'favicon.ico'
@@ -11,3 +19,18 @@ def index():
 @app.get('/favicon.ico')
 async def favicon():
     return FileResponse(favicon_path)
+
+@app.get('/user/{id}')
+def get_user(id):
+    return {'data': id}
+
+@app.get('/blog')
+def item(limit=10, published: bool=True):
+    if published:
+        return {'data': f'{limit}件'}
+    else:
+        return {'data': '非公開'}
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': blog}
