a051c5b8773d177a7724cc2a4ebb660f043176c4
mida-hub <rusuden0106@gmail.com>
2022-09-12 23:42:50 +0900

    rust debug deleted

--- practice/rust/new_crate/target/debug/deps/new_crate.d
+++
@@ -1,5 +0,0 @@
-/Users/ida/github/hobby/practice/rust/new_crate/target/debug/deps/new_crate: src/main.rs
-
-/Users/ida/github/hobby/practice/rust/new_crate/target/debug/deps/new_crate.d: src/main.rs
-
-src/main.rs:
